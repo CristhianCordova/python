18712c01d9341d2a6226b5ad7c220c05a830ba1f
CristhianCordova <40842496+CristhianCordova@users.noreply.github.com>
2022-07-11 22:06:31 -0500

    Add files via upload

---
+++ script1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: cp1252 -*-
+# -*- coding: 850 -*-
+# -*- coding: utf-8 -*-
+
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+
+import pandas as pd
+import os
+
+#Tengo problemas con la tilde
+
+path="I:/Cursos/titanic3.csv"
+
+data = pd.read_csv(path)
+
+data.head()
+
+"Conectarse a base de datos"
+
+
